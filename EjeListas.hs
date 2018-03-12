6b7847a65aa1c89abe8d82cec9ea05bb756f0597
Juan Camilo Espitia <30874068+20152020040@users.noreply.github.com>
2018-03-12 16:52:03 -0500

    Update EjeListas.hs

--- EjeListas.hs
+++ EjeListas.hs
@@ -3,13 +3,13 @@ module EjerListas where
 invert::[Int]->[Int]
 invert [ ] = [ ]
 invert (x:xs) = (invert xs)++[x]
+--sumar pares
 
---Sumar Pares
 sumarPares::[Int]->Int
 sumarPares [] = 0
 sumarPares (x:xs)
-	|(mod x 2) == 0 = sumarPares (xs) +x
-	|otherwise = sumarPares (xs) 
+	|(mod x 2) == 0 = pares (xs) +x
+	|otherwise = pares (xs) 
   
 -- contar pares
 
@@ -23,11 +23,21 @@ pares (x:xs)
 cantimpar::[Int]->Int
 cantimpar []=0
 cantimpar (x:xs)= length [x | x <- (x:xs), mod x 2 ==1]
-  
+
+--contar impares
+
+impares::[Int]->Int
+impares [] = 0
+impares (x:xs)
+	|not ((mod x 2) ==  0) = impares (xs) +1
+	|otherwise = impares (xs) 
+	
 --Mayor de Lista
 mayor::[Int]->Int
 mayor [ ] = 0
 mayor (x:xs)
   |x>mayor(xs)=x
   |otherwise=mayor(xs)
+  
+
     
