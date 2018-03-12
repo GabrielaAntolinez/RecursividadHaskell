f414b5df8443aafb3d90c0cdff92e7796df4da5b
GabrielaAntolinez <36423718+GabrielaAntolinez@users.noreply.github.com>
2018-03-12 16:36:39 -0500

    Update EjeListas.hs

--- EjeListas.hs
+++ EjeListas.hs
@@ -7,7 +7,7 @@ invert (x:xs) = (invert xs)++[x]
 --Cantidad de impares
 cantimpar::[Int]->Int
 cantimpar []=0
-cantimpar lista= length [x | x <- lista, mod x 2 ==1]
+cantimpar (x:xs)= length [x | x <- (x:xs), mod x 2 ==1]
 
 --Mayor de Lista
 mayor::[Int]->Int
