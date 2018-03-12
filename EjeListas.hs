b628ad87d7c535526c2738105d6883fb12214feb
GabrielaAntolinez <36423718+GabrielaAntolinez@users.noreply.github.com>
2018-03-12 16:28:07 -0500

    Add files via upload

---
+++ EjeListas.hs
@@ -0,0 +1,13 @@
+module EjerListas where
+--Invertir lista
+invert::[Int]->[Int]
+invert [ ] = [ ]
+invert (x:xs) = (invert xs)++[x]
+
+
+mayor::[Int]->Int
+mayor [ ] = 0
+mayor (x:xs)
+  |x>mayor(xs)=x
+  |otherwise=mayor(xs)
+    
